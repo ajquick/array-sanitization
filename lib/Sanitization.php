74b8d16121ca00d0199896e67f4a64d742b2bed3
AJ Quick <ajquick@gmail.com>
2017-02-19 23:04:35 -0700

    Update Sanitization
    Add code for ISO8601 sanitization.

--- lib/Sanitization.php
+++ lib/Sanitization.php
@@ -51,15 +51,16 @@ class Sanitization
     public static function sanitizeField($key, $value, $rules)
     {
         if (is_array($value)) {
+			var_export($value);
             return self::sanitize($value, $rules);
         }
 
         if (isset($rules['pattern'])) {
             if ($rules['pattern'] == 'ISO 8601') {
-                $rules['pattern'] = '(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:Z|[+-][01]\d:[0-5]\d)';
+                $value = self::sanitizeISO8601($value);
+            } else {
+                $value = self::sanitizePattern($value, $rules['pattern']);
             }
-
-            $value = preg_replace("/[^(" . $rules['pattern'] . ")]/", "", $value);
         }
 
         if (isset($rules['type'])) {
@@ -111,5 +112,37 @@ class Sanitization
     protected static function sanitizeBoolean($value)
     {
         return filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+    }
+    
+    /**
+     * @param string $value
+     * @return string
+     */
+    protected static function sanitizeISO8601($value)
+    {
+        $pattern = '(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:Z|[+-][01]\d:[0-5]\d)';
+        $value = trim($value);
+         
+        if (preg_match('/^' . $pattern . '$/', $value) !== false ||
+            preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+-]\d{2}:\d{2}$/', $value) !== false || 
+            preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/$', $value) !== false) {
+            return $value;        
+        }
+
+        return preg_replace('/[^0-9TZ\:\-\+]/', '', $value);;
+    }
+    
+    /**
+     * @param string $value
+     * @param string $pattern
+     * @return string
+     */
+    protected static function sanitizePattern($value, $pattern)
+    {
+        if (preg_match('/^' . $pattern . '/$', $value) !== false) {
+            return $value;
+        }
+        
+        return preg_replace('/[^' . $pattern . ']/', '', $value);    
     }
 }
